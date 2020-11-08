fc165a3ee10181d39c81eefc897ab322a00353e9
ozgur <ozgur_1694@hotmail.com>
2020-11-08 22:20:09 +0300

    user card added

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import './UserItem.css';
-const UserItem = props => {
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Cart from "../../shared/components/UIElements/Card";
+import "./UserItem.css";
+
+const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={props.image} alt={props.name}></img>
-        </div>
-        <div>
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+        <Cart className="user-item__content">
+          <Link to={`/${props.id}/places`}>
+            <div className="user-item__image">
+              <Avatar image={props.image} alt={props.name} />
+            </div>
+            <div>
+              <h2>{props.name}</h2>
+              <h3>
+                {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+              </h3>
+            </div>
+          </Link>
+        </Cart>
+      
     </li>
   );
 };
